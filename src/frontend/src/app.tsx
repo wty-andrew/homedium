dd5223b36aa20f0835e04c0c87245542de2b65f8
Andrew <wty.andrew@gmail.com>
2024-09-08 22:11:26 +0800

    :sparkles: (frontend) init

---
+++ src/frontend/src/app.tsx
@@ -0,0 +1,3 @@
+const App = () => <h1 className="text-3xl font-bold underline">Hello World</h1>
+
+export default App
---
+++ src/frontend/vite.config.ts
@@ -0,0 +1,20 @@
+/// <reference types="vitest" />
+
+import path from 'node:path'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './vitest.setup.ts',
+  },
+})
